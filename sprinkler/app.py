335a7809871d3ddca9e77f35e3505c22a446518c
Bear <csaba.ferenclaszlo@zapier.com>
2022-06-21 18:30:05 +0200

    It works on my computer

--- sprinkler/app.py
+++ sprinkler/app.py
@@ -1,12 +1,10 @@
 """default flask app"""
 from flask import Flask
-
+from sprinkler.views import bp
 
 def create_app():
     """Flask application factory
     """
     app = Flask(__name__)
-    @app.route('/')
-    def index():
-        return 'OK'
+    app.register_blueprint(bp)
     return app
