24e13f47cbee7ff901885fded352b16df240a2db
Bear <csaba.ferenclaszlo@zapier.com>
2022-06-23 18:29:12 +0200

    extra debug for stuff module

--- sprinkler/control.py
+++ sprinkler/control.py
@@ -1,6 +1,37 @@
 """Relay control through GPIO"""
+import time
+from os.path import exists
+
 from flask import current_app
-from gpiozero import DigitalOutputDevice
+
+GPIO_BASE_PATH = '/sys/class/gpio'
+
+def gpiopath(pin):
+    """Constructs the /sys fail path to a gpio pin"""
+    return GPIO_BASE_PATH + '/gpio' + str(pin) + '/value'
+
+def initialize_relay_gpio(zone_id):
+    """Makes sure that the relevant GPIO port is exported
+
+    Args:
+        zone_id (_type_): zone id
+    """
+    zones = current_app.config['ZONES']
+    for zone in zones:
+        if zone['id'] == zone_id:
+            if not exists(gpiopath(zone['pin'])):
+                with open(GPIO_BASE_PATH + '/export', 'w', encoding='UTF-8') as gpio_export:
+                    gpio_export.write(str(zone['pin']))
+            directionpath = gpiopath(zone['pin']).replace('value', 'direction')
+            with open(directionpath, 'w', encoding='UTF-8') as gpio_direction:
+                gpio_direction.write('out')
+            for _1 in range(2):
+                with open(gpiopath(zone['pin']), 'w', encoding='UTF-8') as gpio:
+                    gpio.write('1')
+                time.sleep(0.3)
+                with open(gpiopath(zone['pin']), 'w', encoding='UTF-8') as gpio:
+                    gpio.write('0')
+                time.sleep(0.3)
 
 
 def get_relay_status(zone_id):
@@ -16,8 +47,11 @@ def get_relay_status(zone_id):
     zones = current_app.config['ZONES']
     for zone in zones:
         if zone['id'] == zone_id:
-            output = DigitalOutputDevice(zone['pin'], active_high=True, initial_value=None)
-            zone['on'] = bool(output.value)
+            gpio = gpiopath(zone['pin'])
+            with open(gpio, 'r', encoding='UTF-8') as gpio_file:
+                data = gpio_file.read()
+            current_app.logger.info('Read gpio %s, value: %s', zone['pin'], data)
+            zone['on'] = bool(int(data))
             return zone
     return None
 
@@ -35,12 +69,13 @@ def set_relay(zone_id, on): #pylint:disable=invalid-name
     zones = current_app.config['ZONES']
     for zone in zones:
         if zone['id'] == zone_id:
-            output = DigitalOutputDevice(zone['pin'], active_high=True, initial_value=None)
             current_app.logger.info('Switching zone ' + zone['name'] + ' ' + str(on))
-            if on:
-                output.on()
-            else:
-                output.off()
+            gpio = gpiopath(zone['pin'])
+            with open(gpio, 'w', encoding='UTF-8') as gpio_file:
+                if on:
+                    gpio_file.write('1')
+                else:
+                    gpio_file.write('0')
             zone['on'] = on
             return zone
     return None
--- tests/test_control.py
+++ tests/test_control.py
@@ -1,18 +1,65 @@
 import pytest
+import uuid
+import shutil
+from os import makedirs
+from os.path import exists
 from tests.fixtures.test_flask_app import flask_app
-from sprinkler.control import get_relay_status, set_relay
+from sprinkler.control import get_relay_status, set_relay, initialize_relay_gpio
 
-class MockDigitalOutputDevice:
-    value = 1
-    def __init__(self, pin, active_high, initial_value) -> None:
+test_run_id = str(uuid.uuid4())
+
+def test_control_initialize_relay_gpio_already_exported(monkeypatch, flask_app):
+    fake_gpio_base = '/tmp/' + test_run_id
+    if not exists(fake_gpio_base):
+        makedirs(fake_gpio_base)
+    if not exists(fake_gpio_base + '/gpio10'):
+        makedirs(fake_gpio_base + '/gpio10')
+    monkeypatch.setattr('sprinkler.control.GPIO_BASE_PATH', fake_gpio_base)
+    with open(fake_gpio_base + '/export', 'w'):
         pass
-    def on(self):
-        return 1
-    def off(self):
-        return 0
+    with open(fake_gpio_base + '/gpio10/direction', 'w'):
+        pass
+    with open(fake_gpio_base + '/gpio10/value', 'w'):
+        pass
+    with flask_app.app_context():
+        initialize_relay_gpio('1')
+    # with open(fake_gpio_base + '/export', 'r') as f:
+    #     content = f.read()
+    #     assert content == '10'
+    with open(fake_gpio_base + '/gpio10/direction', 'r') as f:
+        content = f.read()
+        assert content == 'out'
+    shutil.rmtree(fake_gpio_base)
+
+def test_control_initialize_relay_gpio_not_exported(monkeypatch, flask_app):
+    fake_gpio_base = '/tmp/' + test_run_id
+    if not exists(fake_gpio_base):
+        makedirs(fake_gpio_base)
+    monkeypatch.setattr('sprinkler.control.GPIO_BASE_PATH', fake_gpio_base)
+    with open(fake_gpio_base + '/export', 'w'):
+        pass
+    with flask_app.app_context():
+        with pytest.raises(FileNotFoundError):
+            initialize_relay_gpio('1')
+    with open(fake_gpio_base + '/export', 'r') as f:
+        content = f.read()
+        assert content == '10'
+    shutil.rmtree(fake_gpio_base)
+
 
 def test_control_get_status(monkeypatch, flask_app):
-    monkeypatch.setattr('sprinkler.control.DigitalOutputDevice', MockDigitalOutputDevice)
+    fake_gpio_base = '/tmp/' + test_run_id
+    if not exists(fake_gpio_base):
+        makedirs(fake_gpio_base)
+    if not exists(fake_gpio_base + '/gpio10'):
+        makedirs(fake_gpio_base + '/gpio10')
+    with open(fake_gpio_base + '/gpio10/value', 'w') as f:
+        f.write('1')
+    if not exists(fake_gpio_base + '/gpio15'):
+        makedirs(fake_gpio_base + '/gpio15')
+    with open(fake_gpio_base + '/gpio15/value', 'w') as f:
+        f.write('0')
+    monkeypatch.setattr('sprinkler.control.GPIO_BASE_PATH', fake_gpio_base)
     with flask_app.app_context():
         assert get_relay_status('1') == {
                 'id': '1',
@@ -24,12 +71,25 @@ def test_control_get_status(monkeypatch, flask_app):
                 'id': '2',
                 'name': 'test zone 2',
                 'pin': 15,
-                'on': True
+                'on': False
             }
         assert get_relay_status('3') == None
+    shutil.rmtree(fake_gpio_base)
+
 
 def test_control_set_status(monkeypatch, flask_app):
-    monkeypatch.setattr('sprinkler.control.DigitalOutputDevice', MockDigitalOutputDevice)
+    fake_gpio_base = '/tmp/' + test_run_id
+    if not exists(fake_gpio_base):
+        makedirs(fake_gpio_base)
+    if not exists(fake_gpio_base + '/gpio10'):
+        makedirs(fake_gpio_base + '/gpio10')
+    with open(fake_gpio_base + '/gpio10/value', 'w') as f:
+        f.write('1')
+    if not exists(fake_gpio_base + '/gpio15'):
+        makedirs(fake_gpio_base + '/gpio15')
+    with open(fake_gpio_base + '/gpio15/value', 'w') as f:
+        f.write('0')
+    monkeypatch.setattr('sprinkler.control.GPIO_BASE_PATH', fake_gpio_base)
     with flask_app.app_context():
         assert set_relay('1', True) == {
                 'id': '1',
@@ -37,10 +97,17 @@ def test_control_set_status(monkeypatch, flask_app):
                 'pin': 10,
                 'on': True
             }
-        assert set_relay('1', False) == {
-                'id': '1',
-                'name': 'test zone 1',
-                'pin': 10,
+        assert set_relay('2', False) == {
+                'id': '2',
+                'name': 'test zone 2',
+                'pin': 15,
                 'on': False
             }
         assert set_relay('3', True) == None
+    with open(fake_gpio_base + '/gpio10/value', 'r') as f:
+        data = f.read()
+        assert bool(int(data)) == True
+    with open(fake_gpio_base + '/gpio15/value', 'r') as f:
+        data = f.read()
+        assert bool(int(data)) == False
+    shutil.rmtree(fake_gpio_base)
